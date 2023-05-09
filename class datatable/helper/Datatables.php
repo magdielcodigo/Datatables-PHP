0d4be69a13b1f6891ad344065631e865971f81e5
magdielcodigo <magdiel.strokes@gmail.com>
2023-05-09 12:33:59 -0500

    adición de mejoras y soporte a sql server antiguo

--- class datatable/helper/Datatables.php
+++ class datatable/helper/Datatables.php
@@ -10,11 +10,15 @@ Class DataTables{
     private $order_column_index;
     private $order_column;
     private $list_columns;
+    private $columns_prepared;
     private $query;
     private $filters;
     private $data;
     private $dict;
     private $cnx;
+    private $db_slc;
+    private $conditions;
+    private $table;
     function __construct($post, $query, $list_columns){
         $this->daw = $post['draw'];
         $this->start = $post['start'];
@@ -23,27 +27,62 @@ Class DataTables{
         $this->order_column_index = $post['order'][0]['column'];
         $this->order_column = $post['order'][0]['dir'];
         $this->list_columns = $list_columns;
-        $this->query = $query;
         $this->filters = '';
         $this->data = array();
         $this->dict = array();
-        $this->cnx = (new Cnx())->connection();
+        $this->db_slc = $config['db_type'];
+        $this->conditions = $config['conditions'] != ''?array('AND '.$config['conditions'], 'WHERE '. $config['conditions']):array('','');        
+        $this->table = $config['table'];
+        if($this->db_slc == 'sqlserver'){
+            $this->cnx = new BDsql($GLOBALS['global_bdarmoniza']);
+            foreach($list_columns as $col){
+                if(strpos(strtoupper($this->table), 'INNER') != false){
+                    $columns_prepared .= $col.",";
+                }else{
+                    $columns_prepared .= "[".$col."],";
+                }
+            }
+            $this->columns_prepared = rtrim($columns_prepared, ",");
+        }else{
+            $this->cnx = (new Cnx())->connection();
+            $this->columns_prepared = "(" . implode(',',$list_columns) . ")";
+        }
+        $this->query = 'SELECT '.$this->columns_prepared.' FROM ' . $config['table'] . ($config['conditions'] != ''?' WHERE '.$config['conditions']:'');
     }
     function process(){
         if($this->search_key){
-            if(!strstr(strtoupper($this->query), 'WHERE')){
+            if(!strstr(strtoupper($this->query), 'WHERE') && $this->db_slc != 'sqlserver'){
                 $this->filters = ' WHERE ';
-                foreach($this->list_columns as $cols){
-                    $this->filters .= $cols . ' = ' . $this->search_key . ' OR ';
-                } 
+            }else if(strstr(strtoupper($this->query), 'WHERE') && $this->db_slc != 'sqlserver'){
+                $this->filters = ' AND ';
             }
-            $res = $this->cnx->query($this->query . rtrim($this->filters, " OR ") . ' LIMIT ' . '10');
+            if($this->db_slc == 'sqlserver'){
+                $this->filters = ' WHERE ';
+            }
+            foreach($this->list_columns as $cols){
+                $this->filters .= $cols . " LIKE '%" . $this->search_key . "%' OR ";
+            }
+            if($this->db_slc == 'sqlserver'){
+                $this->cnx->query('SELECT '.$this->columns_prepared.' FROM '.$this->table.' '.rtrim($this->filters, " OR ") . ' ' . $this->conditions[0]);
+                $this->data = $this->cnx->getResults();
+            }else{
+                $res = $this->cnx->query($this->query . rtrim($this->filters, " OR ") . ' LIMIT ' . '10');
+            }
         }else{
             $column_to_oder = $this->list_columns[$this->order_column_index];
             $order = $this->order_column == 'asc' ? $column_to_oder . ' ASC' : $column_to_oder . ' DESC';
-            $res = $this->cnx->query($this->query . ' ORDER BY ' . $order . ' LIMIT ' . $this->length . ' OFFSET ' . $this->start);
+            if($this->db_slc == 'sqlserver'){
+                $endPagination = $this->start + $this->length;
+                if(((int) $endPagination - 10) != 0){
+                    $endPagination = ($this->start + $this->length) - 1;
+                }
+                $this->cnx->query("SELECT * FROM (SELECT TOP(SELECT COUNT(*) FROM ".$this->table." ".$this->conditions[1].") ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS [Count], ".$this->columns_prepared." FROM ".$this->table." ". $this->conditions[1]. " ORDER BY ".$order.") AS a WHERE ([Count] BETWEEN ".$this->start." AND ".$endPagination.")");
+                $this->data = $this->cnx->getResults();
+            }else{
+                $res = $this->cnx->query($this->query . ' ORDER BY ' . $order . ' LIMIT ' . $this->length . ' OFFSET ' . $this->start);
+            }
         }
-        if(!empty($res) && $res->num_rows > 0){
+        if(!empty($res) && $res->num_rows > 0 && $this->db_slc != 'sqlserver'){
             while($row = $res->fetch_assoc()){
                 $this->data[] = $row;
             }
@@ -51,13 +90,26 @@ Class DataTables{
         $this->returnData();
     }
     function returnData(){
-        $result = $this->cnx->query($this->query);
-        $count_records = $result->num_rows;
-        $this->dict['recordsTotal'] = $count_records ? $count_records : 0; 
-        $this->dict['recordsFiltered'] = $count_records ? $count_records : 0;
+        if($this->db_slc == 'sqlserver'){
+            if($this->search_key){
+                $this->cnx->query('SELECT '.$this->columns_prepared.' FROM '.$this->table.' '.rtrim($this->filters, " OR ").' '.$this->conditions[0]);
+            }else{
+                $this->cnx->query("SELECT COUNT(*) AS 'Count' FROM ".$this->table. " ". $this->conditions[1]);
+            }
+            $count_records = $this->cnx->getResults();
+            $count_records = isset($count_records[0]['Count']) ? $count_records[0]['Count']: 0;
+        }else{
+            if($this->search_key){
+                $result = $this->cnx->query($this->query . rtrim($this->filters, " OR ") . ' LIMIT ' . '10');
+            }else{
+                $result = $this->cnx->query($this->query);
+            }
+            $count_records = $result->num_rows;
+        }
+        $this->dict['recordsTotal'] = $count_records; 
+        $this->dict['recordsFiltered'] = $count_records;
         $this->dict['draw'] = $this->daw;
         $this->dict['data'] = $this->data;
-        echo json_encode($this->dict);
-        $this->cnx->close();
+        echo json_encode($this->dict, JSON_PRETTY_PRINT);
     }
 }
